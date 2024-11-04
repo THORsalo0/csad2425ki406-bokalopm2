8dd96ac8869b411301a45328b2eb0fa4cda04178
THORsalo0 <135055521+THORsalo0@users.noreply.github.com>
2024-11-04 21:51:12 +0200

    123

---
+++ AKPS_Labs/AKPS_Labs/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace AKPS_Labs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.send_button = new System.Windows.Forms.Button();
+            this.input_textBox = new System.Windows.Forms.TextBox();
+            this.output_textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // send_button
+            // 
+            this.send_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.send_button.Location = new System.Drawing.Point(534, 107);
+            this.send_button.Name = "send_button";
+            this.send_button.Size = new System.Drawing.Size(92, 51);
+            this.send_button.TabIndex = 0;
+            this.send_button.Text = "Send";
+            this.send_button.UseVisualStyleBackColor = true;
+            this.send_button.Click += new System.EventHandler(this.send_button_Click);
+            // 
+            // input_textBox
+            // 
+            this.input_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input_textBox.ForeColor = System.Drawing.Color.Gray;
+            this.input_textBox.Location = new System.Drawing.Point(171, 107);
+            this.input_textBox.Multiline = true;
+            this.input_textBox.Name = "input_textBox";
+            this.input_textBox.Size = new System.Drawing.Size(346, 51);
+            this.input_textBox.TabIndex = 2;
+            this.input_textBox.Text = "Put text here...";
+            this.input_textBox.Enter += new System.EventHandler(this.input_textBox_Enter);
+            this.input_textBox.Leave += new System.EventHandler(this.input_textBox_Leave);
+            // 
+            // output_textBox
+            // 
+            this.output_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.output_textBox.Location = new System.Drawing.Point(171, 195);
+            this.output_textBox.Multiline = true;
+            this.output_textBox.Name = "output_textBox";
+            this.output_textBox.ReadOnly = true;
+            this.output_textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.output_textBox.Size = new System.Drawing.Size(455, 191);
+            this.output_textBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.output_textBox);
+            this.Controls.Add(this.input_textBox);
+            this.Controls.Add(this.send_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button send_button;
+        private System.Windows.Forms.TextBox input_textBox;
+        private System.Windows.Forms.TextBox output_textBox;
+    }
+}
+
